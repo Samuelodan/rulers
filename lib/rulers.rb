c44fb17d1e1dfa9fe555d231732a5bc778f11a48
Samuel O'Daniels <35481602+Samuelodan@users.noreply.github.com>
2026-02-24 15:40:40 +0100

    feat: add routing support

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,14 +2,25 @@
 
 # require_relative "rulers/version"
 require "rulers/version"
-require "rulers/array"
+require "rulers/routing"
 
 module Rulers
-	class Application
-		def call(env)
-			`echo debug > debug.txt`
-			[200, {"content-type" => "text/html"},
-				["Hello from Ruby on Rulers!"]]
-		end
-	end
+  class Application
+    def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {"Content-Type" => "text/html"}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
+    end
+  end
 end
