8d09f92ec494150d6b0e52ec485cee97877965fb
Samuel O'Daniels <35481602+Samuelodan@users.noreply.github.com>
2026-02-24 17:04:31 +0100

    feat: temporarily redirect root path to QuotesController#a_quote
    I expect this to change once we have something at the root path.

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -12,6 +12,11 @@ module Rulers
         return [404, {"content-type" => "text/html"}, []]
       end
 
+      if env["PATH_INFO"] == "/"
+        # env["PATH_INFO"] = "/quotes/a_quote" // this is one approach
+        return [302, {"location" => "/quotes/a_quote"}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
